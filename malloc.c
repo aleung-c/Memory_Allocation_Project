108c710f793e1be2362c4c1ca3c63d2e6c0f20bd
AdrienL <aleung-c@e1r11p3.42.fr>
2015-06-05 17:20:21 +0200

    push fin de journee

--- malloc.c
+++ malloc.c
@@ -6,7 +6,7 @@
 /*   By: aleung-c <aleung-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/06/05 10:38:50 by aleung-c          #+#    #+#             */
-/*   Updated: 2015/06/05 15:48:11 by aleung-c         ###   ########.fr       */
+/*   Updated: 2015/06/05 17:20:00 by aleung-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,45 +16,39 @@ void *ft_malloc(size_t size)
 {
 	void *ret;
 
-	if (g_memzone == NULL)
-	{
-		ret = allocate_new_mem(size);
-	}
-	else
-	{
-		ret = search_mem(size);
-	}
+	ret = allocate_new_mem(size);
+	
 	return (ret);
 }
 
 void *allocate_tiny(size_t size)
 {
 	void *ret;
-	t_mem memzone;
 	t_mem *tmp;
 
-	// mmap;
-	ret = mmap(0, TINY,
+	ret = (char *)mmap(0, TINY,
 		PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
 	if (ret == MAP_FAILED)
 	{
 		ft_putendl_fd("ERROR - mmap", 2);
 		return (NULL);
 	}
-	// remplir structure.
-	memzone.ptr = ret + sizeof(t_mem);
-	memzone.size = size;
-	//ft_memcpy(&ret, &memzone, sizeof(t_mem));
-	if (g_memzone == NULL)
-		g_memzone = ret;
+	// cast pour remplir la zone allouée.
+	((t_mem *)ret)->size = size;
+	((t_mem *)ret)->ptr = ret + sizeof(t_mem);
+	if (g_memzone.tiny == NULL)
+	{
+		((t_mem *)ret)->next = NULL;
+		g_memzone.tiny = ret;
+	}
 	else
 	{
-		tmp = g_memzone;
+		tmp = g_memzone.tiny;
 		while (tmp->next)
 			tmp = tmp->next;
 		tmp->next = ret;
 	}
-	return (memzone.ptr);
+	return (((t_mem *)ret)->ptr);
 }
 
 void *allocate_new_mem(size_t size)
@@ -62,9 +56,13 @@ void *allocate_new_mem(size_t size)
 	void *ret;
 
 	ret = NULL;
-	if (size < TINY)
+	if (size < TINY && g_memzone.tiny == NULL)
 	{
 		ret = allocate_tiny(size);
+	}
+	else
+	{
+		ret = search_mem(size);
 	}
 	return (ret);
 }
